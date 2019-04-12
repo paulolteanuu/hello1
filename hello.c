6a019b26f5a29e961cbd12d49e58bc302e4b4dff
Paul Olteanu <paulolteanu12@yahoo.com>
2019-04-12 10:58:48 +0300

    Am scris programul pt salut

--- hello.c
+++ hello.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main()
+int  main ( i n t  argc ,  char
+∗
+argv [ ] )
 {
-     printf("hello world");
-     return 0;
+if  ( argc  != 2)  {
+printf ("USAGE: %s name\n" ,  argv [ 0 ] ) ;
+return -1;
+}
+printf (" Hello , %s !\ n" ,  argv [ 1 ] ) ;
+return  0;
 }
